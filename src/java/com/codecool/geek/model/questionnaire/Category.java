aedc9a2d48f6bc72407d456369ec10c1edd52208
Orsolya Lévai <solya0082@gmail.com>
2018-09-04 19:55:02 +0200

    Category model added

--- src/java/com/codecool/geek/model/questionnaire/Category.java
+++ src/java/com/codecool/geek/model/questionnaire/Category.java
@@ -1,7 +1,59 @@
 package com.codecool.geek.model.questionnaire;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String category;
+
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String image;
+
+    private Category() {
+    }
+
+    public Category(String category, String shortDescription, String image) {
+        this.category = category;
+        this.shortDescription = shortDescription;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
