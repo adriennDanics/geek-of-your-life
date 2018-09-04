01ac38ed06b6d442c7c46c46b77e9b303d9982d6
Orsolya Lévai <solya0082@gmail.com>
2018-09-04 19:32:52 +0200

    Added Answer model

--- src/java/com/codecool/geek/model/questionnaire/Answer.java
+++ src/java/com/codecool/geek/model/questionnaire/Answer.java
@@ -1,7 +1,45 @@
 package com.codecool.geek.model.questionnaire;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @MapsId
+    private Question question;
+
+    private String answer;
+
+
+    public Answer() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
