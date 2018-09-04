ff5daafc6412ee3705c0d2dbca744cf0b38d1190
Adrienn <adrienn.d91@gmail.com>
2018-09-04 14:54:03 +0200

    Gender, user and question models completed

--- src/java/com/codecool/geek/model/customer/User.java
+++ src/java/com/codecool/geek/model/customer/User.java
@@ -1,7 +1,49 @@
 package com.codecool.geek.model.customer;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/java/com/codecool/geek/model/questionnaire/Question.java
+++ src/java/com/codecool/geek/model/questionnaire/Question.java
@@ -1,7 +1,54 @@
 package com.codecool.geek.model.questionnaire;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String question;
+
+    @OneToMany(mappedBy = "")
+    private Set<Answer> answers = new HashSet<>();
+
+    public Question(String question, Set<Answer> answers) {
+        this.question = question;
+        this.answers = answers;
+    }
+
+    public Question() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Set<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Set<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public void addAnswer(Answer answer){
+        answers.add(answer);
+    }
 }
