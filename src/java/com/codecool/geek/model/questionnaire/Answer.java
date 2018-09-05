4a96bc280bfd0459340e9b58e596eff2b31b7998
Orsolya Lévai <solya0082@gmail.com>
2018-09-05 09:15:11 +0200

    Changed long to Long in methods

--- src/java/com/codecool/geek/model/questionnaire/Answer.java
+++ src/java/com/codecool/geek/model/questionnaire/Answer.java
@@ -10,7 +10,7 @@ public class Answer {
     private Long id;
 
     @ManyToOne
-    @MapsId
+    //@MapsId
     private Question question;
 
     private String answer;
@@ -24,11 +24,11 @@ public class Answer {
         this.answer = answer;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
